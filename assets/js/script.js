45a63f9e1a930957bb992ccc7de78e2572020bb1
Lpcsouza <17843059+lpcsouza@users.noreply.github.com>
2024-11-17 23:16:48 -0300

    realizado algumas alteracoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,5 @@ menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open")
 
   const isOpen = navLinks.classList.contains("open")
-  menuBtn.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
 })
