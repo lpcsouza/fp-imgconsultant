a1ca551bd736a66e639f4434b189811dff90b1a1
Luiz Paulo <luizpaulocs@gmail.com>
2024-11-19 17:59:45 -0300

    add img and new script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,42 @@
-const menuBtn = document.getElementById("menu-btn")
-const navLinks = document.getElementById("nav-links")
-const menuBtnIcon = menuBtn.querySelector("i")
+const nav = document.querySelector(".nav")
+const btnMenu = document.querySelector(".btn-menu")
+const menu = document.querySelector(".menu")
 
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open")
+function handleButtonClick(event) {
+  if (event.type === "touchstart") event.preventDefault()
+  event.stopPropagation()
+  nav.classList.toggle("active")
+  handleClickOutside(menu, () => {
+    nav.classList.remove("active")
+  })
+}
 
-  const isOpen = navLinks.classList.contains("open")
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
-})
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute("data-target")
+      html.removeEventListener("click", handleHTMLClick)
+      html.removeEventListener("touchstart", handleHTMLClick)
+      callback()
+    }
+  }
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("click", handleHTMLClick)
+    html.addEventListener("touchstart", handleHTMLClick)
+    targetElement.setAttribute("data-target", "")
+  }
+}
+
+function setAria() {
+  const isActive = nav.classList.contains("active")
+  btnMenu.setAttribute("aria-expanded", isActive)
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Close Menu")
+  } else {
+    btnMenu.setAttribute("aria-label", "Open Menu")
+  }
+}
+
+btnMenu.addEventListener("click", handleButtonClick)
+btnMenu.addEventListener("touchstart", handleButtonClick)
